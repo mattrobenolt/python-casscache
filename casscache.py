4f0a581ca830f252753dc39f02fc0c6db3f399d8
Matt Robenolt <matt@ydekproductions.com>
2013-08-10 16:35:45 -0700

    Handle get_many and sets with TTL

--- casscache.py
+++ casscache.py
@@ -1,5 +1,22 @@
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, Session
 from cassandra.query import ValueSequence
+
+def _execute_many(self, statements):
+    futures = []
+    results = []
+
+    for statement in statements:
+        futures.append(self.execute_async(statement))
+
+    for future in futures:
+        try:
+            results.append(future.result())
+        except Exception:
+            results.append(None)
+
+    return results
+
+Session.execute_many = _execute_many
 
 
 class Client(object):
@@ -18,22 +35,25 @@ class Client(object):
         self._session.set_keyspace(self.keyspace)
 
         # Prepare all of the necessary statements beforehand
-        self._GET_STATEMENT = self._session.prepare("SELECT value, flags FROM %s WHERE key = ? LIMIT 1" % self.column_family)
-        self._SET_STATEMENT = self._session.prepare("INSERT INTO %s (key, value, flags) VALUES (?, ?, ?)" % self.column_family)
+        self._GET = self._session.prepare("SELECT value, flags FROM %s WHERE key = ? LIMIT 1" % self.column_family)
+        self._SET = self._session.prepare("INSERT INTO %s (key, value, flags) VALUES (?, ?, ?)" % self.column_family)
+        # Cannot be prepared with a dynamic TTL pre C* 2.0
+        # See https://issues.apache.org/jira/browse/CASSANDRA-4450
+        self._SET_TTL = "INSERT INTO %s (key, value, flags) VALUES (?, ?, ?) USING TTL %%d" % self.column_family
 
     def get(self, key):
-        statement = self._GET_STATEMENT
-        try:
-            return self._session.execute(statement.bind((key,)))[0].value
-        except IndexError:
-            return None
-
+        statement = self._GET
+        return self._handle_row(self._session.execute(statement.bind((key,))))
 
     def get_multi(self, keys):
-        pass
+        statement = self._GET
+        return map(self._handle_row, self._session.execute_many((statement.bind((key,)) for key in keys)))
 
     def set(self, key, value, ttl=0):
-        statement = self._SET_STATEMENT
+        if ttl == 0:
+            statement = self._SET
+        else:
+            statement = self._session.prepare(self._SET_TTL % ttl)
         self._session.execute(statement.bind((key, value, 0)))
         return True
 
@@ -59,3 +79,9 @@ class Client(object):
     def flush_all(self):
         query = "TRUNCATE %s" % self.column_family
         self._session.execute(query)
+
+    def _handle_row(self, rows):
+        try:
+            return rows[0].value
+        except (IndexError, TypeError):
+            return None
